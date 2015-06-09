576aafcec46ffc1cb5839dc5c26a4a2a2d3b3fa7
Josh Wykell <jwykell@gmail.com>
2015-06-09 07:41:41 -0400

    models, schema, and controlers created

---
+++ db/migrate/20150609112223_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.references :post, index: true, foreign_key: true
+      t.string :author
+      t.string :email
+      t.string :body
+
+      t.timestamps null: false
+    end
+  end
+end
