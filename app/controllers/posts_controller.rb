aff0b5364316aab20735764e8c71fff5820b476a
Josh Wykell <jwykell@gmail.com>
2015-06-11 08:28:40 -0400

    athentication works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # before_action :require_authenticated_user :except => [:index]
+  before_action :require_authenticated_user, :except => [:index]
 
   def index
     @posts = Post.all
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     email, password = params.values_at(:email, :password)
     user = User.find_by(email: email).try(:authenticate, password)
 
-    if user
+    if user 
       session[:user_id] = user.id
       flash[:notice] = "#{user.name} logged in."
     else
